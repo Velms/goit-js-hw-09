c86f35d8dff99947dd95cb13bf0f44de8973df2d
Velms <78916991+Velms@users.noreply.github.com>
2023-09-02 18:27:32 +0300

    1.0

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+let intervalId = null;
+
+function onStart() {
+  refs.startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStop() {
+  refs.startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
